00d6799d9433585a78ce32ba4f914ac32a005e14
Ashutosh Sharma <108739465+Ashutosh-Sharma-14@users.noreply.github.com>
2023-05-02 09:52:15 +0530

    Update ripemd.py

--- ripemd.py
+++ ripemd.py
@@ -6,11 +6,13 @@ def ripemd128(message):
     message = bytearray(message)
     message_len = len(message)
     message.append(0x80)
+#     0x80 is the hexadecimal representation of 10000000 - this marks the start of the padding bit
     while (len(message) % 64) != 56:
+#         if the length of the block is not a multiple of 64 append padding bits at the end
         message.append(0x00)
     message += message_len.to_bytes(8, byteorder='little')
     
-    # Initial hash values
+    # Initial hash values - chaining variables
     h0 = 0x67452301
     h1 = 0xEFCDAB89
     h2 = 0x98BADCFE
@@ -18,11 +20,14 @@ def ripemd128(message):
     
     # Constants
     k = [0x00000000]*16 + [0x5A827999]*16 + [0x6ED9EBA1]*16 + [0x8F1BBCDC]*16 + [0xA953FD4E]*16
+#     These constants are pre-defined as 32-bit integers and are unique to the RIPEMD-128 algorithm. 
+# They are used in the message expansion step to generate the 80 words needed for each of the 4 rounds.
     r = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
          [7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8],
          [3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12],
          [1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2],
          [4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13]]
+#     shift specified in each round
     s = [[11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8],
          [7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12],
          [11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5],
